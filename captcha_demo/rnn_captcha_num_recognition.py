c145f699e811d6dea2e3ff052a87c8ac991cdb2d
tensorflow1 <tensorflow1@liev.top>
2018-07-18 17:03:40 +0800

    Captcha: Add RNN version captcha recognition, but can't normal train

--- captcha_demo/rnn_captcha_num_recognition.py
+++ captcha_demo/rnn_captcha_num_recognition.py
@@ -33,6 +33,8 @@ def gen_captcha_text_and_image():
     # image.write(captcha_text, captcha_text + '.jpg')  # 写到文件
 
     captcha_image = Image.open(captcha)
+    w, h = captcha_image.size
+    captcha_image.thumbnail((w // 2, h // 2))
     captcha_image = np.array(captcha_image)
     return captcha_text, captcha_image
 
@@ -54,11 +56,11 @@ def get_next_batch(batch_size=128):
     batch_x = np.zeros([batch_size, IMAGE_HEIGHT * IMAGE_WIDTH])
     batch_y = np.zeros([batch_size, MAX_CAPTCHA * CHAR_SET_LEN])
 
-    # 有时生成图像大小不是(60, 160, 3)
+    # 有时生成图像大小不是(30, 80, 3)
     def wrap_gen_captcha_text_and_image():
         while True:
             text, image = gen_captcha_text_and_image()
-            if image.shape == (60, 160, 3):
+            if image.shape == (30, 80, 3):
                 return text, image
 
     for i in range(batch_size):
@@ -116,42 +118,26 @@ class RNNCaptchaRecognition:
         self.Y = tf.placeholder(tf.float32, [None, MAX_CAPTCHA * CHAR_SET_LEN])
         self.keep_prob = tf.placeholder(tf.float32)  # dropout
 
-        self.w_c1 = tf.Variable(tf.truncated_normal([3, 3, 1, 32], stddev=0.1))
-        self.b_c1 = tf.Variable(tf.zeros([32]))
+        self.w_d = tf.Variable(tf.truncated_normal([128, 64], stddev=0.1))
+        self.b_d = tf.Variable(tf.zeros(64))
 
-        self.w_c2 = tf.Variable(tf.truncated_normal([3, 3, 32, 64], stddev=0.1))
-        self.b_c2 = tf.Variable(tf.zeros([64]))
-
-        self.w_c3 = tf.Variable(tf.truncated_normal([3, 3, 64, 128], stddev=0.1))
-        self.b_c3 = tf.Variable(tf.zeros([128]))
-
-        self.w_d = tf.Variable(tf.truncated_normal([8 * 20 * 128, 1024], stddev=0.1))
-        self.b_d = tf.Variable(tf.zeros(1024))
-
-        self.w_out = tf.Variable(tf.truncated_normal([1024, MAX_CAPTCHA * CHAR_SET_LEN], stddev=0.1))
+        self.w_out = tf.Variable(tf.truncated_normal([64, MAX_CAPTCHA * CHAR_SET_LEN], stddev=0.1))
         self.b_out = tf.Variable(tf.zeros([MAX_CAPTCHA * CHAR_SET_LEN]))
 
     def forward(self):
-        x = tf.reshape(self.X, shape=[-1, IMAGE_HEIGHT, IMAGE_WIDTH, 1])
+        # [100, 80, 30]
+        x = tf.reshape(self.X, shape=[-1, IMAGE_WIDTH, IMAGE_HEIGHT])
 
-        # 3 conv layer
-        conv1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x, self.w_c1, strides=[1, 1, 1, 1], padding='SAME'), self.b_c1))
-        conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        # conv1 = tf.nn.dropout(conv1, keep_prob)
-
-        conv2 = tf.nn.relu(
-            tf.nn.bias_add(tf.nn.conv2d(conv1, self.w_c2, strides=[1, 1, 1, 1], padding='SAME'), self.b_c2))
-        conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        # conv2 = tf.nn.dropout(conv2, keep_prob)
-
-        conv3 = tf.nn.relu(
-            tf.nn.bias_add(tf.nn.conv2d(conv2, self.w_c3, strides=[1, 1, 1, 1], padding='SAME'), self.b_c3))
-        conv3 = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-        # conv3 = tf.nn.dropout(conv3, keep_prob)
+        # cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(layer) for layer in (128, 256)])
+        cell = tf.contrib.rnn.BasicLSTMCell(128)
+        init_state = cell.zero_state(100, dtype=tf.float32)
+        ys, _ = tf.nn.dynamic_rnn(cell, x, initial_state=init_state, time_major=False)
+        # ys: [100, 80, 128] --> y: [100, 128]
+        y = ys[:, -1, :]
 
         # Fully connected layer
-        dense = tf.reshape(conv3, [-1, self.w_d.get_shape().as_list()[0]])
-        dense = tf.nn.relu(tf.add(tf.matmul(dense, self.w_d), self.b_d))
+        dense = tf.reshape(y, [-1, self.w_d.get_shape().as_list()[0]])
+        dense = tf.nn.sigmoid(tf.add(tf.matmul(dense, self.w_d), self.b_d))
         dense = tf.nn.dropout(dense, self.keep_prob)
 
         self.out = tf.add(tf.matmul(dense, self.w_out), self.b_out)
@@ -176,10 +162,10 @@ if __name__ == '__main__':
     # Initial some necessary parameters for functions
     text, image = gen_captcha_text_and_image()
     # 图像大小
-    IMAGE_HEIGHT = 60
-    IMAGE_WIDTH = 160
+    IMAGE_HEIGHT = 30
+    IMAGE_WIDTH = 80
     MAX_CAPTCHA = len(text)
-    print("验证码图像channel:", image.shape)  # (60, 160, 3)
+    print("验证码图像channel:", image.shape)  # (30, 80, 3)
     # 验证码最长4字符; 我全部固定为4,可以不固定. 如果验证码长度小于4，用'_'补齐
     print("验证码文本最长字符数", MAX_CAPTCHA)
 
@@ -204,9 +190,9 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         sess.run(init)
         for epoch in range(1000000):
-            batch_x, batch_y = get_next_batch(128)
-            _, loss, acc = sess.run([recnet.optimizer, recnet.loss, recnet.accuracy],
-                                    feed_dict={recnet.X: batch_x, recnet.Y: batch_y, recnet.keep_prob: 0.75})
+            batch_x, batch_y = get_next_batch(100)
+            out_test, _, loss, acc = sess.run([recnet.out, recnet.optimizer, recnet.loss, recnet.accuracy],
+                                              feed_dict={recnet.X: batch_x, recnet.Y: batch_y, recnet.keep_prob: 0.75})
 
             # 每100 step计算一次准确率
             if epoch % 100 == 0:
@@ -215,11 +201,11 @@ if __name__ == '__main__':
                 if acc > 0.9:
                     saver.save(sess, "./crack_capcha.ckpt", global_step=epoch)
 
-                # Test the model
-                batch_x_test, batch_y_test = get_next_batch(10)
-                # saver.restore(sess, tf.train.latest_checkpoint('.'))
-                out_test = sess.run(recnet.out, feed_dict={recnet.X: batch_x_test, recnet.keep_prob: 1.})
-
+                # # Test the model
+                # batch_x_test, batch_y_test = get_next_batch(100)
+                # # saver.restore(sess, tf.train.latest_checkpoint('.'))
+                # out_test = sess.run(recnet.out, feed_dict={recnet.X: batch_x_test, recnet.keep_prob: 1.})
+                #
                 out_test = np.argmax(np.reshape(out_test, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), axis=2)
                 # print(out_test)
                 for i in range(len(out_test)):
@@ -229,4 +215,4 @@ if __name__ == '__main__':
                     for n in out_t:
                         vector[j * CHAR_SET_LEN + n] = 1
                         j += 1
-                    print("Label: {}, predict: {}".format(vec2text(batch_y_test[i]), vec2text(vector)))
+                    print("Label: {}, predict: {}".format(vec2text(batch_y[i]), vec2text(vector)))

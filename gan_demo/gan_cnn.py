b591608eb5fb755daa5b23abdfa19a7dcd07550f
tensorflow1 <tensorflow1@liev.top>
2018-07-26 14:38:05 +0800

    GAN: Add batch norm function to gan_cnn.py

--- gan_demo/gan_cnn.py
+++ gan_demo/gan_cnn.py
@@ -6,6 +6,15 @@ import matplotlib.pyplot as plt
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
+def bn(x, is_training):
+    return tf.contrib.layers.batch_norm(x,
+                                        decay=0.9,
+                                        updates_collections=None,
+                                        epsilon=1e-5,
+                                        scale=True,
+                                        is_training=is_training)
+
+
 class GNet:
 
     def __init__(self):
@@ -18,17 +27,20 @@ class GNet:
             self.conv1_w = tf.Variable(tf.truncated_normal([3, 3, 16, 32], stddev=0.1, dtype=tf.float32))
             self.conv2_w = tf.Variable(tf.truncated_normal([3, 3, 1, 16], stddev=0.1, dtype=tf.float32))
 
-    def forward(self, x):
-        y = tf.nn.leaky_relu(tf.matmul(x, self.w1) + self.b1)
-        y = tf.nn.leaky_relu(tf.matmul(y, self.w2) + self.b2)
+    def forward(self, x, is_training, reuse=False):
+        # with tf.variable_scope("gnetf", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope("gnetf", reuse=reuse):
+            y = tf.nn.leaky_relu(bn(tf.matmul(x, self.w1) + self.b1, is_training=is_training))
+            y = tf.nn.leaky_relu(bn(tf.matmul(y, self.w2) + self.b2, is_training=is_training))
 
-        y = tf.reshape(y, [-1, 7, 7, 32])
-        deconv1 = tf.nn.conv2d_transpose(y, self.conv1_w, output_shape=[100, 14, 14, 16], strides=[1, 2, 2, 1],
-                                         padding='SAME')
-        deconv2 = tf.nn.conv2d_transpose(deconv1, self.conv2_w, output_shape=[100, 28, 28, 1], strides=[1, 2, 2, 1],
-                                         padding='SAME')
+            y = tf.reshape(y, [-1, 7, 7, 32])
+            deconv1 = tf.nn.leaky_relu(
+                bn(tf.nn.conv2d_transpose(y, self.conv1_w, output_shape=[100, 14, 14, 16], strides=[1, 2, 2, 1],
+                                          padding='SAME'), is_training=is_training))
+            deconv2 = tf.nn.conv2d_transpose(deconv1, self.conv2_w, output_shape=[100, 28, 28, 1], strides=[1, 2, 2, 1],
+                                             padding='SAME')
 
-        return deconv2
+            return deconv2
 
     def getParam(self):
         return tf.get_collection(tf.GraphKeys.VARIABLES, scope="gnet")
@@ -51,25 +63,29 @@ class DNet:
 
             self.w2 = tf.Variable(tf.truncated_normal([128, 1], stddev=0.1))
 
-    def forward(self, x):
-        conv1 = tf.nn.relu(tf.nn.conv2d(x, self.conv1_w, strides=[1, 1, 1, 1],
-                                        padding='SAME') + self.conv1_b)
-        pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                               padding='SAME')
+    def forward(self, x, is_training, reuse=False):
+        # with tf.variable_scope("dnetf", reuse=tf.AUTO_REUSE):
+        with tf.variable_scope("dnetf", reuse=reuse):
+            conv1 = tf.nn.leaky_relu(tf.nn.conv2d(x, self.conv1_w, strides=[1, 2, 2, 1],
+                                                  padding='SAME') + self.conv1_b)
+            # conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+            #                        padding='SAME')
 
-        conv2 = tf.nn.relu(
-            tf.nn.conv2d(pool1, self.conv2_w, strides=[1, 1, 1, 1], padding='SAME') + self.conv2_b)
-        pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            conv2 = tf.nn.leaky_relu(
+                bn(tf.nn.conv2d(conv1, self.conv2_w, strides=[1, 2, 2, 1], padding='SAME') + self.conv2_b,
+                   is_training=is_training))
+            # conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-        flat = tf.reshape(pool2, [-1, 7 * 7 * 32])
+            flat = tf.reshape(conv2, [-1, 7 * 7 * 32])
 
-        y = tf.matmul(flat, self.w1) + self.b1
-        tf.summary.histogram("Discriminator pure output", y)
-        y0 = tf.nn.relu(y)
-        y1 = tf.nn.dropout(y0, keep_prob=0.9)
-        y_out = tf.matmul(y1, self.w2)
+            y = tf.matmul(flat, self.w1) + self.b1
+            tf.summary.histogram("Discriminator pure output", y)
+            y = bn(y, is_training=is_training)
+            y0 = tf.nn.relu(y)
+            # y1 = tf.nn.dropout(y0, keep_prob=0.9)
+            y_out = tf.matmul(y0, self.w2)
 
-        return y_out
+            return y_out
 
     def getParam(self):
         return tf.get_collection(tf.GraphKeys.VARIABLES, scope="dnet")
@@ -84,29 +100,40 @@ class Net:
         self.g_x = tf.placeholder(dtype=tf.float32, shape=[None, 128])
         self.f_y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
 
+        self.learning_rate = 0.0002
+        self.beta1 = 0.5
+
         self.gnet = GNet()
         self.dnet = DNet()
 
         self.forward()
         self.backward()
+        self.test()
 
     def forward(self):
-        self.r_d_out = self.dnet.forward(self.r_x)
+        self.r_d_out = self.dnet.forward(self.r_x, is_training=True, reuse=tf.AUTO_REUSE)
 
-        self.g_out = self.gnet.forward(self.g_x)
+        self.g_out = self.gnet.forward(self.g_x, is_training=True, reuse=tf.AUTO_REUSE)
         print("g_out shape: ", self.g_out.shape)
-        self.g_d_out = self.dnet.forward(self.g_out)
+        self.g_d_out = self.dnet.forward(self.g_out, is_training=True, reuse=tf.AUTO_REUSE)
 
     def backward(self):
         self.d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.r_d_out, labels=self.t_y)) \
                       + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.g_d_out, labels=self.f_y))
-
         tf.summary.scalar("Discriminator loss", self.d_loss)
-        self.d_opt = tf.train.AdamOptimizer(0.0001).minimize(self.d_loss, var_list=self.dnet.getParam())
 
         self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.g_d_out, labels=self.t_y))
         tf.summary.scalar("Generator loss", self.g_loss)
-        self.g_opt = tf.train.AdamOptimizer(0.0001).minimize(self.g_loss, var_list=self.gnet.getParam())
+
+        # optimizers
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            self.d_opt = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).minimize(self.d_loss,
+                                                                                               var_list=self.dnet.getParam())
+            self.g_opt = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).minimize(self.g_loss,
+                                                                                               var_list=self.gnet.getParam())
+
+    def test(self):
+        self.test_g_out = self.gnet.forward(self.g_x, is_training=False, reuse=tf.AUTO_REUSE)
 
 
 if __name__ == '__main__':
@@ -138,11 +165,15 @@ if __name__ == '__main__':
 
             if epoch % 100 == 0:
                 print("epoch: {}, d_loss: {}, g_loss: {}".format(epoch, _d_loss, _g_loss))
-                # test_xs = np.random.uniform(-1, 1, (100, 128))
-                # imgs = sess.run(net.g_out, feed_dict={net.g_x: test_xs})
+                test_xs = np.random.uniform(-1, 1, (100, 128))
+                test_imgs = sess.run(net.test_g_out, feed_dict={net.g_x: test_xs})
+                test_img = np.reshape(test_imgs[0], (28, 28))
                 img = np.reshape(imgs[0], (28, 28))
-                # print(img)
+
                 plt.clf()
+                plt.subplot(211)
                 plt.imshow(img)
-                plt.pause(1)
+                plt.subplot(212)
+                plt.imshow(test_img)
+                plt.pause(0.5)
         plt.ioff()
